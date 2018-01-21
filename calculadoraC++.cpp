bd87c45e120576be90c7bf4e998bac371e1b237a
Unknown <idv17c.rcharreton@uartesdigitales.edu.mx>
2018-01-21 10:36:00 -0600

    Una Manera mas sencilla
    Se modificaron las funciones paraque se manden a llamar desde su funcion,

--- calculadoraC++.cpp
+++ calculadoraC++.cpp
@@ -3,46 +3,140 @@ using namespace std;
 
 /* Esta funcion realiza la operacion de suma
 con la declaracion  de dos valores a y b */
+
+
 int suma(int a, int b)
 {
 	int result;
-	result = a + b;
+	int valorA = 0;
+	int valorB = 0;
+	// Operacion de suma
+	cout << "== SUMA ==" << endl;
+	cout << "Ingresa el primer valor de la suma: " << endl;
+	cin >> valorA;
+	cout << "Ingresa el segundo valor de la suma: " << endl;
+	cin >> valorB;
+
+	result = valorA + valorB;
+	cout << "\0" << endl;
+	if (!cin)
+	{
+		cout << "Es necesario escribir un numero" << endl;
+		cin.clear();
+		cin.ignore();
+		cin.get();
+	}
 	return result;
 }
 
 /* Esta funcion realiza la operacion de multiplicacion
 con la declaracion  de dos valores c y d */
-int multiplicacion(int c, int  d)
+int multiplicacion(int a, int  b)
 {
 	int result;
-	result = c * d;
+	int valorA = 0;
+	int valorB = 0;
+	// Operacion de multiplicacion
+	cout << "== MULTIPLICACION ==" << endl;
+
+	cout << "Ingrese el primer valor de la multiplicacion: " << endl;
+	cin >> valorA;
+	cout << "Ingrese el segundo valor de la multiplicacion: " << endl;
+	cin >> valorB;
+
+
+	cout << "\0" << endl;
+	result = valorA * valorB;
+	if (!cin)
+	{
+		cout << "Es necesario escribir un numero" << endl;
+		cin.clear();
+		cin.ignore();
+		cin.get();
+	}
 	return result;
 }
 
 /* Esta funcion realiza la operacion de la division
 con la declaracion  de dos valores e y f */
-int division(int e, int f)
+int division(int a, int b)
 {
 	int result;
-	result = e / f;
+	int valorA = 0;
+	int valorB = 0;
+
+	// Operacion de division
+	cout << "== DIVISION ==" << endl;
+
+	cout << "Ingrese el divisor de la division: " << endl;
+	cin >> valorA;
+	cout << "Ingrese el dividendo de la division: " << endl;
+	cin >> valorB;
+
+	cout << "\0" << endl;
+	result = valorA / valorB;
+	if (!cin)
+	{
+		cout << "Es necesario escribir un numero" << endl;
+		cin.clear();
+		cin.ignore();
+		cin.get();
+	}
 	return result;
 }
 
 /* Esta funcion realiza la operacion de modulo
 con la declaracion  de dos valores g y h */
-int modulo(int g, int h)
+int modulo(int a, int b)
 {
 	int result;
-	result = g % h;
+	int valorA = 0;
+	int valorB = 0;
+	// Operacion de modulo
+	cout << "== MODULO ==" << endl;
+
+	cout << "Ingrese el divisor de la division: " << endl;
+	cin >> valorA;
+	cout << "Ingrese el valor del dividendo: " << endl;
+	cin >> valorB;
+
+
+	cout << "\0" << endl;
+
+	result = valorA % valorB;
+	if (!cin)
+	{
+		cout << "Es necesario escribir un numero" << endl;
+		cin.clear();
+		cin.ignore();
+		cin.get();
+	}
 	return result;
 }
 
 /* Esta funcion realiza la operacion de resta
 con la declaracion  de dos valores i y j */
-int resta(int i, int j)
+int resta(int a, int b)
 {
 	int result;
-	result = i - j;
+	int valorA = 0;
+	int valorB = 0;
+	// Operacion de resta
+	cout << "== RESTA ==" << endl;
+
+	cout << "Ingrese el primer valor de la resta: " << endl;
+	cin >> valorA;
+	cout << "Ingrese el segundo valor de la resta: " << endl;
+	cin >> valorB;
+	cout << "\0" << endl;
+	result = valorA - valorB;
+	if (!cin)
+	{
+		cout << "Es necesario escribir un numero" << endl;
+		cin.clear();
+		cin.ignore();
+		cin.get();
+	}
 	return result;
 }
 
@@ -51,7 +145,25 @@ con la declaracion  de dos valores numero y potencia */
 int potencia(int numero, int potencia)
 {
 	int result;
+	int valorNum = 0;
+	int valorPot = 0;
+	// Operacion de potencia 
+	cout << "== POTENCIA ==" << endl;
+
+	cout << "Ingrese el numero: " << endl;
+	cin >> numero;
+	cout << "Ingrese la potencia: " << endl;
+	cin >> potencia;
+
+	cout << "\0" << endl;
 	result = pow(numero, potencia);
+	if (!cin)
+	{
+		cout << "Es necesario escribir un numero" << endl;
+		cin.clear();
+		cin.ignore();
+		cin.get();
+	}
 	return result;
 }
 
@@ -59,6 +171,8 @@ int potencia(int numero, int potencia)
 /* Aqui se inicializaran las funciones */
 int main()
 {
+
+
 	// Titulo 
 	cout << "=====================================" << endl;
 	cout << "==== Bienvenido a la calculadora ====" << endl;
@@ -68,88 +182,53 @@ int main()
 	cout << "Estas son las funciones que se pueden realizar:" << endl;
 	cout << "\0" << endl;
 
-	cout << "- Suma" << endl;
-	cout << "- Multiplicacion" << endl;
-	cout << "- Division" << endl;
-	cout << "- Modulo" << endl;
-	cout << "- Resta" << endl;
-	cout << "- Potencia" << endl;
+	cout << "- Suma (1)" << endl;
+	cout << "- Multiplicacion (2)" << endl;
+	cout << "- Division (3)" << endl;
+	cout << "- Modulo (4)" << endl;
+	cout << "- Resta (5)" << endl;
+	cout << "- Potencia (6)" << endl;
 
 	cout << "\0" << endl;
-	// Variables
-	int valorA = 0;
-	int valorB = 0;
-	int valorC = 0;
-	int valorD = 0;
-	int valorE = 0;
-	int valorF = 0;
-	int valorG = 0;
-	int valorH = 0;
-	int valorI = 0;
-	int valorJ = 0;
-	int valorNum = 0;
-	int valorPot = 0;
+	// Aqui inicia la magia del programa
+	// Se pregunta que funcion se quiere realizar
 
-	// Operacion de suma
-	cout << "== SUMA ==" << endl;
+	int opcion = 0;
+	cout << "Escriba el numero de la funcion: " << endl;
+	cin >> opcion;
+	if (opcion == 1) // Resultado de la suma
+	{
+		cout << "Su resultado es: " << suma(0, 0) << endl;
+	}
+	if (opcion == 2) // Resultado de la multiplicacion
+	{
+		cout << "El resultado de la multiplicacion es: " << multiplicacion(0, 0) << endl;
+	}
+	if (opcion == 3) // Resultado de la division
+	{
+		cout << "El resultado de la division es: " << division(0, 0) << endl;
+	}
+	if (opcion == 4) // Resultado Modulo
+	{
+		cout << "El modulo de la divison es: " << modulo(0, 0) << endl;
+	}
+	if (opcion == 5) // Resultado Resta
+	{
+		cout << "El resultado de la resta es: " << resta(0, 0) << endl;
+	}
+	if (opcion == 6) // Resultado Potencia
+	{
+		cout << "El resultado de la potencia es: " << potencia(0, 0) << endl;
+	}
 
-	cout << "Ingresa el primer valor de la suma: " << endl;
-	cin >> valorA;
-	cout << "Ingresa el segundo valor de la suma: " << endl;
-	cin >> valorB;
-	cout << "Su resultado es: " << suma(valorA, valorB) << endl;
 
-	cout << "\0" << endl;
-	// Operacion de multiplicacion
-	cout << "== MULTIPLICACION ==" << endl;
-
-	cout << "Ingrese el primer valor de la multiplicacion: " << endl;
-	cin >> valorC;
-	cout << "Ingrese el segundo valor de la multiplicacion: " << endl;
-	cin >> valorD;
-	cout << "El resultado de la multiplicacion es: " << multiplicacion(valorC, valorD) << endl;
-
-	cout << "\0" << endl;
-	// Operacion de division
-	cout << "== DIVISION ==" << endl;
-
-	cout << "Ingrese el divisor de la division: " << endl;
-	cin >> valorE;
-	cout << "Ingrese el dividendo de la division: " << endl;
-	cin >> valorF;
-	cout << "El resultado de la division es: " << division(valorE, valorF) << endl;
-
-	cout << "\0" << endl;
-	// Operacion de modulo
-	cout << "== MODULO ==" << endl;
-
-	cout << "Ingrese el divisor de la division: " << endl;
-	cin >> valorG;
-	cout << "Ingrese el valor del dividendo: " << endl;
-	cin >> valorH;
-	cout << "El modulo de la divison es: " << modulo(valorG, valorH) << endl;
-
-	cout << "\0" << endl;
-	// Operacion de resta
-	cout << "== RESTA ==" << endl;
-
-	cout << "Ingrese el primer valor de la resta: " << endl;
-	cin >> valorI;
-	cout << "Ingrese el segundo valor de la resta: " << endl;
-	cin >> valorJ;
-	cout << "El resultado de la resta es: " << resta(valorI, valorJ) << endl;
-
-	cout << "\0" << endl;
-	// Operacion de potencia 
-	cout << "== POTENCIA ==" << endl;
-
-	cout << "Ingrese el numero: " << endl;
-	cin >> valorNum;
-	cout << "Ingrese la potencia: " << endl;
-	cin >> valorPot;
-	cout << valorNum << " elevado a la " << valorPot << " es igual a: " << potencia(valorNum, valorPot);
-
-	cout << "\0" << endl;
+	if (!opcion)
+	{
+		cout << "Es necesario escribir un numero" << endl;
+		cin.clear();
+		cin.ignore();
+		cin.get();
+	}
 
 	cin.ignore();
 	cin.get();
