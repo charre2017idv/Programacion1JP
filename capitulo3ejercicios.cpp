18e13e9aabbe6a41e0d80898e81675c222d0c50f
Unknown <idv17c.rcharreton@uartesdigitales.edu.mx>
2018-01-21 21:28:19 -0600

    Capitulo 3 - Ejercicio 3
    Se realizo el ejercico tres de variables

---
+++ capitulo3ejercicios.cpp
@@ -0,0 +1,25 @@
+// Capitulo 3 - Ejercicio 2
+#include<iostream>
+#include<math.h>
+
+//Variables legales
+int edad = 18;
+unsigned int alumnos = 13;
+long numerote = 98272364626721;
+float calificacion = 8.5f;
+double calculoFisico = 8.9;
+char letra = 'a';
+
+//Variables ilegales
+int double = 0;
+int unsigned = 0;
+long float = 0l
+doble char letra = 'a';
+
+
+int main()
+{
+	
+
+	return 0;
+}
