ce8d4d33411f05fc983ef85498f18d91dc8dd3d9
Unknown <idv17c.rcharreton@uartesdigitales.edu.mx>
2018-01-21 23:57:31 -0600

    Adelanto Ejercico 4
    Es un adelanto del Ejercicio 4 del capitulo 3

---
+++ capitulo3ejercicio4.cpp
@@ -0,0 +1,61 @@
+// Capitulo 3 - Ejercico 4
+/* Write a program that prompts the user to enter two integer values. Store
+these values in int variables named val1 and val2. Write your program to
+determine the smaller, larger, sum, difference, product, and ratio of these
+values and report them to the user */
+#include<iostream>
+
+using namespace std;
+
+int calculatodo(int Valor1, int Valor2)
+{
+	int Valor1;
+	int Valor2;
+	int mayor;
+	int menor;
+	int suma;
+	int resta;
+
+	cout << "Ingresa el primer valor: " << endl;
+	cin >> Valor1;
+	cout << "Ingresa el segundo valor: " << endl;
+	cin >> Valor2;
+
+	if (Valor1 > Valor2)
+	{
+		mayor = Valor1;
+		menor = Valor2;
+	}
+	else
+	{
+		menor = Valor1;
+		mayor = Valor2;
+	}
+
+	suma = Valor1 + Valor2;
+	resta = Valor1 - Valor2;
+	
+
+	cout << "El mayor es: " << mayor << endl;
+	cout << "El menor es: " << menor << endl;
+	cout << "El resultado de la suma es: " << suma << endl;
+	cout << "El resultado de la resta es: " << resta << endl;
+	return 0;
+}
+
+
+
+int main()
+{
+	cout << "=====================================" << endl;
+	cout << "==== Bienvenido a la calculadora ====" << endl;	cout << "=====================================" << endl;	cout << "- Calcula todo (1)" << endl;
+	int opcion = 0;
+	cout << "Escriba el numero de la funcion: " << endl;
+	cin >> opcion;
+	if (opcion == 1)
+		cout << calculatodo(0,0) << endl;
+		
+	cin.ignore();
+	cin.get();
+	return 0;
+}
