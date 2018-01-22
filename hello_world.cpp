30ba6e20ee67f1eb65ef799bc87fd4b16e32bb12
Unknown <idv17c.rcharreton@uartesdigitales.edu.mx>
2018-01-21 18:35:16 -0600

    Ejercico 1 y Drill 1
    Se realizaron los ejercicios del capitulo 2

---
+++ hello_world.cpp
@@ -0,0 +1,69 @@
+// Capitulo 2 - Drill 1
+
+#include <iostream>
+
+using namespace std;
+inline void keep_window_open() { char ch; cin >> ch; }
+/*
+int main() // C++ programs start by executing the function main
+{
+	cout << "Hello, World!\n"; // output “Hello, World!”
+	keep_window_open(); // wait for a character to be entered
+	return 0;
+}
+*/
+// Capitulo 2 - Exercises
+
+int main()
+{
+	// Ejercicio 1
+	cout << "Hello World!" << endl;
+	cout << "Here we go!" << endl;
+	// Ejercicio 2
+	cout << "===================================================" << endl;
+	cout << "=== Instrucciones para llegar al baño de arriba ===" << endl;
+	cout << "===================================================" << endl;
+
+	cout << "===      Descripcion del edificio de la UAD     ===" << endl;
+	cout << "Tu posicion actual es la recepcion que se encuentra al sur del sanitario," << endl;
+	cout << "existen dos posibilidades para entrar llegar al bano." << endl;
+	cout << "La primera opcion es por las escaleras que se encuentran saliendo de la recepcion" << endl;
+	cout << "La segunda opcion es por las escaleras que estan enfrente del Aula 2" << endl;
+
+	cout << "===================================================" << endl;
+	cout << "Por fines practicos utilizaremos la segunda opcion." << endl;
+	cout << "===================================================" << endl;
+	cout << "=====   Pasos para llegar a los sanitarios    =====" << endl;
+	cout << "Paso 1) Fijar la ubicacion de las escaleras." << endl;
+	cout << "Paso 2) Caminar 20 pasos a paso tranquilo " << endl;
+	cout << "Paso 3) Detenerse tras los 20 pasos." << endl;
+	cout << "Paso 4) Dar vuelta a la izquierda." << endl;
+	cout << "Paso 5) Subir las primeras escaleras y deterse." << endl;
+	cout << "Paso 7) Dar vuelta a la derecha y subir los escalones restantes." << endl;
+	cout << "Paso 8) Estando ya en el segundo piso, dar vuelta a la derecha." << endl;
+	cout << "Paso 9) Dar 5 pasos y dar vuelta a la izquierda." << endl;
+	cout << "Paso 10) Dar 5 pasos y detenerse enfrente de los sanitarios." << endl;
+	cout << "Paso 11) Dirijade al sanitario de su prefirencia y Let it Go!" << endl;
+
+	// Ejercicio 3
+	cout << "==========================================================" << endl;
+	cout << "=== Instrucciones para llegar al salon desde mi cuarto ===" << endl;
+	cout << "==========================================================" << endl;
+
+	cout << "===      Descripcion del recorrido     ===" << endl;
+	cout << "Tu posicion actual es mi cuarto en el segundo piso de mi casa." << endl;
+	cout << "El salon de clases al que ssistire es el laboratorio 1." << endl;
+	cout << "===================================================" << endl;
+	cout << "====   Pasos para llegar al laboratorio 5      ====" << endl;
+	cout << "===================================================" << endl;
+	cout << "Paso 1) Caminar 10 pasos hacia enfrente y girar a la derecha." << endl;
+	cout << "Paso 2) Bajar las escaleras y girar a la izquierda." << endl;
+	cout << "Paso 3) Caminar 6 pasos y girar a la izquierda." << endl;
+	cout << "Paso 4) Abrir la puerta y cerrala al salir." << endl;
+	cout << "Paso 5) Subase al coche y maneje por 45 minutos hacia la escuela. " << endl;
+	cout << "Paso 6) Estacionese y camine hacia la escuela. " << endl;
+	cout << "Paso 7) Entre a la UAD y suba las escaleras que estan a mano izquierda. " << endl;
+	cout << "Paso 8) De Vuelta a la izquierda y dirijase hasta topar con el Laboratorio 5." << endl;
+	keep_window_open();
+	return 0;
+}
